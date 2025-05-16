252270740bf5cab050cef7a16a8d4e612067a954
Reinel David Hernandez <davidhernandez3884@gmail.com>
2025-05-16 11:59:03 -0500

    FORM

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 import { esValido } from "./modulo.js";
-
-// Variables
+//Variables
 const formulario = document.querySelector("form");
 const nombre = document.querySelector("[name=nombre]");
 const apellido = document.querySelector("[name=apellido]");
@@ -10,12 +9,9 @@ const usuario = document.querySelector("[name=usuario]");
 const contrasena = document.querySelector("[name=contrasena]");
 const politicas = document.querySelector("[name=politicas]");
 const boton = document.querySelector("#btn_validar");
-const usuarios = document.querySelector("#usuarios");
-const ciudadSelect = document.getElementById("ciudad");
-const generoDiv = document.querySelector(".form_control .form_radio");
-const lenguajesDiv = document.getElementById("lenguajes");
 
-// Funciones de validación
+
+//Funciones
 const validar = (event) => {
   const valores = [nombre, apellido, telefono, documento, usuario, contrasena];
   const valoresString = [
@@ -33,21 +29,60 @@ const validar = (event) => {
   for (let x = 0; x < valores.length; x++) {
     if (valores[x].value.trim() === "") {
       if (valores[x].nextElementSibling) valores[x].nextElementSibling.remove();
-      if (cont > 0) mensaje += ", " + valoresString[x];
-      else mensaje += " " + valoresString[x];
-      cont++;
+      if (cont > 0) {
+        mensaje += ", " + valoresString[x];
+      } else {
+        mensaje += " " + valoresString[x];
+        cont++;
+      }
       valores[x].style.border = "2px solid red";
-      if (focus === 0) valores[x].focus();
-      focus++;
+      if (focus == 0) {
+        valores[x].focus();
+        focus++;
+      }
       let aviso = document.createElement("span");
       aviso.classList.add("avisoError");
       aviso.textContent = `El campo ${valoresString[x]} es Obligatorio`;
       valores[x].insertAdjacentElement("afterend", aviso);
     }
+    // else formulario.addEventListener("submit", esValido);
   }
-  if (mensaje !== "Ingrese correctamente: ") alert(mensaje);
+  if (mensaje != "Ingrese correctamente: ") alert(mensaje);
 };
-
+const letras = (event) => {
+  const regexp = /^[a-zA-Z]$/;
+  if (
+    !regexp.test(event.key) &&
+    event.key !== "Backspace" &&
+    event.key !== "Tab"
+  ) {
+    event.preventDefault();
+  }
+  if (event.target.value.length >= 10) {
+    event.preventDefault();
+  }
+};
+const numeros = (event) => {
+  if (
+    !/^\d$/.test(event.key) &&
+    event.key !== "Backspace" &&
+    event.key !== "Tab"
+  ) {
+    event.preventDefault();
+  }
+  if (event.target.value.length >= 10) {
+    event.preventDefault();
+  }
+};
+const limpiar = (event) => {
+  if (event.target.value !== "") {
+    event.target.classList.remove("error");
+    event.target.style.border = "0px";
+    if (event.target.nextElementSibling) {
+      event.target.nextElementSibling.remove();
+    }
+  }
+};
 const acepta = () => {
   if (!politicas.checked) {
     boton.setAttribute("disabled", "");
@@ -55,13 +90,104 @@ const acepta = () => {
     boton.removeAttribute("disabled");
   }
 };
-
 const isValid = (e) => {
   let data = esValido(e);
   console.log(data);
 };
-
-// Eventos
+//Eventos
 addEventListener("DOMContentLoaded", acepta);
 politicas.addEventListener("change", acepta);
 formulario.addEventListener("submit", isValid);
+// formulario.addEventListener("submit", validar);
+// nombre.addEventListener("keydown", letras);
+// apellido.addEventListener("keydown", letras);
+// telefono.addEventListener("keydown", numeros);
+// documento.addEventListener("keydown", numeros);
+// nombre.addEventListener("blur", limpiar);
+// apellido.addEventListener("blur", limpiar);
+// telefono.addEventListener("blur", limpiar);
+// documento.addEventListener("blur", limpiar);
+// usuario.addEventListener("blur", limpiar);
+// contrasena.addEventListener("blur", limpiar);
+
+
+
+const posts = async () => {
+  const request = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts = await request.json();
+  return posts;
+};
+
+const usuarios = async () => {
+  const request = await fetch("https://jsonplaceholder.typicode.com/users");
+  const usuarios = await request.json();
+  return usuarios;
+};
+
+const tabla = (posts, usuarios) => {
+  const root = document.querySelector("#app");
+  const tabla = document.createElement("table");
+  const header = document.createElement("thead");
+  const thUsuario = document.createElement("th");
+  const thTitulo = document.createElement("th");
+  const thcuerpo = document.createElement("th");
+  const thacciones = document.createElement("th");
+  const tbody = document.createElement("tbody");
+
+  const fragmento = document.createDocumentFragment();
+  posts.forEach(({ id, userId, title, body }) => {
+    const { name } = usuarios.find((elemento) => elemento.id == userId);
+
+    const tr = document.createElement("tr");
+    const tdUsuario = document.createElement("td");
+    const tdTitulo = document.createElement("td");
+    const tdBody = document.createElement("td");
+    const tdAcciones = document.createElement("td");
+    const btnEditar = document.createElement("button");
+    const btnEliminar = document.createElement("button");
+    btnEditar.setAttribute("data-id", id);
+    btnEliminar.setAttribute("data-id", id);
+    tr.setAttribute("id", `post_${id}`);
+    tdUsuario.textContent = name;
+    tdTitulo.textContent = title;
+    tdBody.textContent = body;
+    btnEditar.textContent = "Editar";
+    btnEliminar.textContent = "Eliminar";
+    btnEditar.classList.add("editar");
+    btnEliminar.classList.add("eliminar");
+    tdAcciones.append(btnEditar, btnEliminar);
+    tr.append(tdUsuario, tdTitulo, tdBody, tdAcciones);
+    fragmento.append(tr);
+  });
+  tbody.append(fragmento);
+
+  thUsuario.textContent = "Usuario";
+  thTitulo.textContent = "Titulo";
+  thcuerpo.textContent = "Cuerpo";
+  thacciones.textContent = "Acciones";
+
+  header.append(thUsuario, thTitulo, thcuerpo, thacciones);
+  tabla.append(header, tbody);
+
+  root.append(tabla);
+};
+
+const data = Promise.all([posts(), usuarios()]).then((data) => {
+  const [posts, usuarios] = data;
+  tabla(posts, usuarios);
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target.matches(".editar")) {
+    let id = e.target.dataset.id;
+    alert(id);
+  }
+  if (e.target.matches(".eliminar")) {
+    let id = e.target.dataset.id;
+    let tr = document.querySelector(`#post_${id}`);
+    tr.remove();
+    console.log(tr);
+
+    alert(id);
+  }
+});
--- modulo.js
+++ modulo.js
@@ -1,78 +1,41 @@
 export const esValido = (e) => {
   e.preventDefault();
+  // console.log(e.target.children);
   const obj = {};
   const campos = [...e.target].filter((elemento) => {
     return elemento.hasAttribute("required");
   });
-
-  const radios = [...campos].filter((elemento) => elemento.type === "radio");
-  const checkbox = [...campos].filter(
-    (elemento) => elemento.type === "checkbox"
-  );
-
+  const radios = [...campos].filter((elemento) => {
+    return elemento.type === "radio";
+  });
+  const checkbox = [...campos].filter((elemento) => {
+    return elemento.type === "checkbox";
+  });
   const campo_radio = radios.find((radio) => radio.checked) || [];
   if (campo_radio.length === 0) {
-    obj[radios[0]?.name] = "";
+    obj[radios[0].name] = "";
   } else {
-    obj[radios[0]?.name] = campo_radio.value;
+    obj[radios[0].name] = campo_radio.value;
   }
-
   const campo_checkbox = checkbox.filter((e) => e.checked);
   if (campo_checkbox.length < 3) {
-    obj[checkbox[0]?.name] = "";
+    obj[checkbox[0].name] = "";
   } else {
-    obj[checkbox[0]?.name] = [...campo_checkbox].map((e) => e.value);
+    obj[checkbox[0].name] = [...campo_checkbox].map((e) => e.value);
   }
 
   campos.forEach((campo) => {
     switch (campo.tagName) {
       case "INPUT":
         if (
-          campo.type === "text" ||
-          campo.type === "number" ||
-          campo.type === "password" ||
-          campo.type === "tel"
+          campo.type == "text" ||
+          campo.type == "number" ||
+          campo.type == "password" ||
+          campo.type == "tel"
         ) {
           obj[campo.name] = campo.value;
-
-          // Validación específica según el tipo de input
           if (campo.value.trim() === "") {
             campo.classList.add("error");
-          } else {
-            campo.classList.remove("error");
-            switch (campo.type) {
-              case "text":
-                if (!/^[a-zA-Z\s]*$/.test(campo.value)) {
-                  campo.classList.add("error");
-                  alert(`El campo ${campo.name} solo debe contener letras.`);
-                }
-                break;
-              case "number":
-                if (!/^\d*$/.test(campo.value)) {
-                  campo.classList.add("error");
-                  alert(`El campo ${campo.name} solo debe contener números.`);
-                }
-                break;
-              case "password":
-                if (!/^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]*$/.test(campo.value)) {
-                  campo.classList.add("error");
-                  alert(
-                    `El campo ${campo.name} debe contener letras y números.`
-                  );
-                }
-                break;
-              case "tel":
-                if (!/^[0-9+\-()\s]*$/.test(campo.value)) {
-                  campo.classList.add("error");
-                  alert(
-                    `El campo ${campo.name} debe contener un formato telefónico válido.`
-                  );
-                }
-                break;
-
-              default:
-                break;
-            }
           }
         }
         break;
@@ -80,15 +43,10 @@ export const esValido = (e) => {
         obj[campo.name] = campo.selectedIndex;
         if (campo.selectedIndex === 0) {
           campo.classList.add("error");
-        } else {
-          campo.classList.remove("error");
         }
-        break;
-
       default:
         break;
     }
   });
-
   return obj;
 };
